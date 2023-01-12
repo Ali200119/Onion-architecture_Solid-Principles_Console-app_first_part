d7af443489c6c4b929ef535c5a6a661b649a28a6
Ali200119 <aliit@code.edu.az>
2023-01-12 04:24:56 +0400

    feat: AppDbContext

---
+++ LibraryApp/Repository/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Repository.DAL
+{
+	public static class AppDbContext<T>
+	{
+		public static List<T> datas;
+
+		static AppDbContext()
+		{
+			datas = new List<T>();
+		}
+	}
+}
