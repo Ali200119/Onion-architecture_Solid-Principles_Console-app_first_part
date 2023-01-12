5f78b2f3a5d889bcf7a9deaca4e29ba16fe82549
Ali200119 <aliit@code.edu.az>
2023-01-12 04:52:34 +0400

    feat: LibraryRepository

---
+++ LibraryApp/Repository/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Domain.Common;
+
+namespace Repository.Repositories.Interfaces
+{
+	public interface IRepository<T> where T: BaseEntity
+	{
+		void Create(T entity);
+		void Delete(T entity);
+		void Update(T entity);
+		T Get(Predicate<T> predicate);
+		List<T> GetAll(Predicate<T> predicate);
+	}
+}
